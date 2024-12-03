b4e42e0d9fc0882ab8226682b198491fd84a37a3
Gold-17 <goldkenwaydavidson17@gmail.com>
2024-12-03 13:37:53 +0100

    Updated the sidebar's padding on mobile view

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
         <aside className="w-full h-full bg-neutral-100 p-4 flex flex-col hide-scrollbar">
             <Link
               href="/"
-              className="flex mt-0 items-center gap-x-2 sm:mt-6"
+              className="flex items-center gap-x-2 mt-0 sm:mt-6"
             >
                 <Image
                   src="/workaura-logo.svg"

eebd7c57187f26b84f3a6d68d170fc79d23f48a8
Gold-17 <goldkenwaydavidson17@gmail.com>
2024-12-03 12:03:03 +0100

    Updated the sign up and sign in page

--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -4,24 +4,22 @@ import * as z from "zod";
 import Link from "next/link";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
 import { Loader } from "lucide-react";
 
 import DottedSeparator from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
 } from "@/components/ui/card";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
@@ -36,172 +34,169 @@ const SignUpCard = () => {
     defaultValues: {
       name: "",
       email: "",
-      password: ""
-    }
+      password: "",
+    },
   });
 
   const onSubmit = (values: z.infer<typeof registerSchema>) => {
     mutate({
-      json: values
+      json: values,
     });
   };
 
-    return (
-        <Card
-          className="w-full h-full md:w-[486px] border-none shadow-none"
+  return (
+    <Card
+      className="w-full h-full md:w-[486px] border-none shadow-none"
+    >
+      <CardHeader
+        className="flex items-center justify-center text-center p-7"
+      >
+        <CardTitle
+          className="text-2xl font-extrabold tracking-normal"
         >
-            <CardHeader
-              className="flex items-center justify-center text-center p-7"
-            >
-                <CardTitle
-                  className="text-2xl font-extrabold tracking-normal"
-                >
-                    Create an account
-                </CardTitle>
-            </CardHeader>
+          Create an account
+        </CardTitle>
+      </CardHeader>
 
-            <div
-              className="px-7"
-            >
-                <DottedSeparator />
-            </div>
+      <div
+        className="px-7"
+      >
+        <DottedSeparator />
+      </div>
 
-            <CardContent
-              className="p-7"
-            >
-              <Form
-                {...form}
-              >
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-4 mb-3"
-                >
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type="text"
-                            placeholder="Enter your name"
-                            disabled={isPending}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type="email"
-                            placeholder="Enter your email"
-                            disabled={isPending}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type="password"
-                            placeholder="Enter your password"
-                            disabled={isPending}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button
-                    className="w-full rounded-lg"
-                    size="lg"
-                    disabled={isPending}
-                  >
-                    {isPending && (
-                      <Loader
-                        className="w-4 h-4 mr-2 animate-spin"
-                      />
-                    )}
-                    Sign Up
-                  </Button>
-                </form>
-              </Form>
-            </CardContent>
-
-            <div
-              className="px-4"
-            >
-                <DottedSeparator />
-            </div>
-
-            <CardContent
-              className="flex flex-col p-7 gap-y-4"
-            >
-                {/* <Button
-                  className="w-full rounded-lg"
-                  variant="secondary"
-                  size="lg"
-                  disabled={isPending}
-                >
-                    <FcGoogle
-                      className="mr-5 size-5"
+      <CardContent
+        className="p-7"
+      >
+        <Form
+          {...form}
+        >
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mb-3"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Enter your name"
+                      disabled={isPending}
                     />
-                    Continue with Google
-                </Button> */}
-                <Button
-                  className="w-full rounded-lg"
-                  variant="secondary"
-                  size="lg"
-                  disabled={isPending}
-                >
-                    <FaGithub
-                      className="mr-5 size-5"
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="Enter your email"
+                      disabled={isPending}
                     />
-                    Continue with Github
-                </Button>
-            </CardContent>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      placeholder="Enter your password"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="w-full rounded-lg"
+              size="lg"
+              disabled={isPending}
+            >
+              {isPending && <Loader className="w-4 h-4 mr-2 animate-spin" />}
+              Sign Up
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
 
-            <div
-              className="px-7"
+      <div
+        className="px-4"
+      >
+        <DottedSeparator />
+      </div>
+
+      {/* <CardContent
+        className="flex flex-col p-7 gap-y-4"
+      >
+        <Button
+          className="w-full rounded-lg"
+          variant="secondary"
+          size="lg"
+          disabled={isPending}
+        >
+          <FcGoogle
+            className="mr-5 size-5"
+          />
+          Continue with Google
+        </Button>
+        <Button
+          onClick={() => signUpWithGithub()}
+          className="w-full rounded-lg"
+          variant="secondary"
+          size="lg"
+          disabled={isPending}
+        >
+          <FaGithub
+            className="mr-5 size-5"
+          />
+          Continue with Github
+        </Button>
+      </CardContent>
+
+      <div
+        className="px-7"
+      >
+        <DottedSeparator />
+      </div> */}
+
+      <CardContent
+        className="flex items-center justify-center p-7"
+      >
+        <p
+          className="text-xs font-normal"
+        >
+          Already have an account?{" "}
+          <Link
+            href="/sign-in"
+          >
+            <span
+              className="text-[#0077b6]"
             >
-                <DottedSeparator />
-            </div>
-            
-            <CardContent
-              className="flex items-center justify-center p-7"
-            >
-                <p
-                  className="text-xs font-normal"
-                >
-                    Already have an account?{" "}
-                    <Link
-                      href="/sign-in"
-                    >
-                        <span
-                          className="text-[#0077b6]"
-                        >
-                            Sign In
-                        </span>
-                    </Link>
-                </p>
-            </CardContent>
-        </Card>
-    );
-}
- 
+              Sign In
+            </span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
 export default SignUpCard;
